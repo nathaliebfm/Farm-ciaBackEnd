391c65be582cc882628ccff38ad48a5a8998e2e9
nathaliebfm <natibfm@hotmail.com>
2022-10-06 14:31:17 -0300

    Construção do back-end de um sistema destinado à organização de estoque de farmácia

---
+++ src/main/java/com/drugstore/repository/ProdutosRepository.java
@@ -0,0 +1,16 @@
+package com.drugstore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.drugstore.model.ProdutosModel;
+
+@Repository
+public interface ProdutosRepository extends JpaRepository<ProdutosModel, Long>{
+	
+	public List<ProdutosModel> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
